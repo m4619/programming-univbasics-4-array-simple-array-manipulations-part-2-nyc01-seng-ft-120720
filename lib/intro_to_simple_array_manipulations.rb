064f2eba85f5b36297093f3f4971bd2309338740
Miguel  Arreola <marreola4619@gmail.com>
2020-11-09 21:48:10 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(my_favorite_things, more_favs)
+  all_my_favs = my_favorite_things.concat more_favs
+end
+
+def using_insert(array, element)
+ 
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq 
+end
+
+def using_flatten(instruments)
+  instruments.flatten 
+end
+
+def using delete(instructors, string)
+  instructors.delete
+end 
