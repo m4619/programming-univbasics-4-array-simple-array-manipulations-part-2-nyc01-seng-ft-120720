490f954c15ee610bd6f26732439b855fe1884292
Miguel  Arreola <marreola4619@gmail.com>
2020-11-09 21:50:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, element)
- list_of_programming_languages.insert()
+ list_of_programming_languages.insert(4, "Python")
 end
 
 def using_uniq(haircuts)
